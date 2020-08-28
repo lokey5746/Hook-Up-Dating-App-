722de23043de7954cc30bb07d40aeb30c24ff09d
Lokendra Dangi <pachar.com@gmail.com>
2020-08-28 14:17:08 +0545

    Complete Header and setup Routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
 import React from "react";
 import "./App.css";
+import Header from "./Header";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <h2>Hook Up</h2>
+      <Header />
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <h1>I am homepage</h1>
+          </Route>
+          <Route path="/chat">
+            <h1>I am chat section</h1>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
 
 export default App;
+
+{
+  /* Tinder card section */
+}
+{
+  /* Button card section */
+}
+
+{
+  /* All chat section */
+}
+{
+  /* Single chat section*/
+}
---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDgwV0zkuoh4W1U0nJxMSg6VnkgYGWr3oE",
+  authDomain: "hook-up-8e74a.firebaseapp.com",
+  databaseURL: "https://hook-up-8e74a.firebaseio.com",
+  projectId: "hook-up-8e74a",
+  storageBucket: "hook-up-8e74a.appspot.com",
+  messagingSenderId: "1005271912136",
+  appId: "1:1005271912136:web:46ee8ee7fc8b3512495ffd",
+};
+
+//Initialize firebase App
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+//Accessing firebase firestore database
+const db = firebaseApp.firestore();
+
+export default db;
