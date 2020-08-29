e198f8f73d9928585391e5b962f48b3d20d55046
Lokendra Dangi <pachar.com@gmail.com>
2020-08-29 11:44:04 +0545

    Logo Update

--- src/Header.js
+++ src/Header.js
@@ -5,6 +5,7 @@ import IconButton from "@material-ui/core/IconButton";
 import ArrowBackIosIcon from "@material-ui/icons/ArrowBackIos";
 import "./Header.css";
 import { Link, useHistory } from "react-router-dom";
+import Hook from "./Hook Up.png";
 
 const Header = ({ backButton }) => {
   const history = useHistory();
@@ -20,11 +21,7 @@ const Header = ({ backButton }) => {
         </IconButton>
       )}
       <Link to="/">
-        <img
-          className="header__logo"
-          src="https://1000logos.net/wp-content/uploads/2018/07/tinder-logo.png"
-          alt="tinder logo"
-        />
+        <img className="header__logo" src={Hook} alt="tinder logo" />
       </Link>
       <Link to="/chat">
         <IconButton>
